924b4842588e37f611d158523d64eaee60579d7d
syed-zunair-gillani <zunairgillani54@gmail.com>
2024-06-08 16:26:23 +0500

    1st

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+import Image from "next/image";
+import Button from '@mui/material/Button';
+
+export default function Home() {
+  return (
+    <Button variant="contained">Hello world</Button>
+  );
+}
