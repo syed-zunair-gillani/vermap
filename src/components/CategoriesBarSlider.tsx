6e48fe37a9ef10f86ddf7c0d0134f15763350827
syed-zunair-gillani <dev.zunaair@gmail.com>
2024-09-07 17:31:41 +0500

    update

--- src/components/CategoriesBarSlider.tsx
+++ src/components/CategoriesBarSlider.tsx
@@ -3,30 +3,36 @@ import Image from "next/image";
 import React, { useContext, useState } from "react";
 import Slider from "react-slick";
 import Container from "./ui/container";
-import {cateorySettings} from '@/utils'
+import { cateorySettings } from "@/utils";
 import { GlobalContext } from "@/context/global-context";
+import { useRouter } from "next/navigation";
 
 const CategoriesBarSlider = () => {
   const slider = React.useRef<any>(null);
-  const {setCategory, setListing, listing, category} = useContext(GlobalContext)
-  const [items, setItems] = useState(listing)
+  const { setCategory, setListing, listing, category } =
+    useContext(GlobalContext);
+  const [items, setItems] = useState(listing);
+  const router = useRouter();
 
-  const handleListingCategory = (slug:any) => {
-    const filterListing = items?.filter((item:any)=>item.category === slug)
-    setListing(filterListing)
-    setCategory(slug)
-  }
+  const handleListingCategory = (slug: any) => {
+    const filterListing = items?.filter((item: any) => item.category === slug);
+    setListing(filterListing);
+    setCategory(slug);
+    // router.push(`?category=${slug}`);
+  };
 
   return (
-    <section className="border-t-[1px] py-4 sticky top-[70px] z-10 bg-white">
-      <Container >
+    <section className="border-t-[1px] pt-4 pb-[1px] sticky top-[70px] z-10 bg-white">
+      <Container>
         <div className="relative">
           <Slider {...cateorySettings} ref={slider} className="pl-5 pr-8">
             {categoriesList?.map((item: any, idx: number) => (
               <button
                 key={idx}
-                className={`opacity-60 outline-none hover:opacity-100 cursor-pointer !flex flex-col items-center border-transparent justify-center ${category === item.slug && "opacity-100"}`}
-                onClick={()=>handleListingCategory(item?.slug)}
+                className={`opacity-60 outline-none hover:opacity-100 cursor-pointer !flex flex-col items-center border-transparent justify-center ${
+                  category === item.slug && "!opacity-100"
+                }`}
+                onClick={() => handleListingCategory(item?.slug)}
               >
                 <Image
                   src={item.icon}
@@ -35,7 +41,13 @@ const CategoriesBarSlider = () => {
                   height={32}
                   className=""
                 />
-                <h6 className={`text-[13px] border-b-[2px] border-transparent pb-2 ${category === item.slug && "border-black"}`}>{item?.title}</h6>
+                <h6
+                  className={`text-[13px] border-b-[2px] border-transparent pb-2 ${
+                    category === item.slug && "!border-black"
+                  }`}
+                >
+                  {item?.title}
+                </h6>
               </button>
             ))}
           </Slider>
@@ -44,14 +56,33 @@ const CategoriesBarSlider = () => {
             className="absolute top-0 h-full bg-gradient-to-l from-white/0 via-white to-white pr-10"
           >
             <div className="p-1 border rounded-full">
-            <svg width="1.4em" height="1.4em" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="m14 7l-5 5l5 5"/></svg>            </div>
+              <svg width="1.4em" height="1.4em" viewBox="0 0 24 24">
+                <path
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="1.5"
+                  d="m14 7l-5 5l5 5"
+                />
+              </svg>{" "}
+            </div>
           </button>
           <button
             onClick={() => slider?.current?.slickNext()}
             className="absolute right-0 top-0 bg-gradient-to-r from-white/0 via-white to-white h-full pl-10"
           >
             <div className="p-1 border rounded-full">
-                <svg width="1.4em" height="1.4em" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="m10 17l5-5l-5-5"/></svg>
+              <svg width="1.4em" height="1.4em" viewBox="0 0 24 24">
+                <path
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="1.5"
+                  d="m10 17l5-5l-5-5"
+                />
+              </svg>
             </div>
           </button>
         </div>
@@ -61,8 +92,6 @@ const CategoriesBarSlider = () => {
 };
 
 export default CategoriesBarSlider;
-
-
 
 const categoriesList = [
   {
--- src/components/Item-listing/ItemListing.tsx
+++ src/components/Item-listing/ItemListing.tsx
@@ -1,12 +1,11 @@
 "use client"
 import React, { useContext, useState } from 'react'
 import ItemCard from './Item-card'
-import { GlobalContext } from '@/context/global-context'
 
 const ItemListing = ({data}:any) => {
   
   return (
-    <div className='grid grid-cols-2 md:grid-cols-3 2xl:grid-cols-4 3xl:grid-cols-5 gap-3 md:gap-4 lg:gap-5'>
+    <div id="listing" className='grid grid-cols-2 md:grid-cols-3 2xl:grid-cols-4 3xl:grid-cols-5 gap-3 md:gap-4 lg:gap-5 mt-4'>
         {
             data?.map((item:any,idx:number)=>(
               <ItemCard data={item} key={idx}/>
