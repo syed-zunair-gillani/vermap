94142aa41ba2ce08bbc06160b1a80f3c3af4720c
syed-zunair-gillani <dev.zunaair@gmail.com>
2024-09-07 15:52:10 +0500

    blogs pages

---
+++ src/app/blogs/[slug]/layout.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SingleBlogLayout = ({children}:any) => {
+  return (
+    <div>{children}</div>
+  )
+}
+
+export default SingleBlogLayout
---
+++ src/app/blogs/[slug]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SingleBlogPage = () => {
+  return (
+    <div>SingleBlogPage</div>
+  )
+}
+
+export default SingleBlogPage
