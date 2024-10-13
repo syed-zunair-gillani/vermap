0048e08480c8591749b35cc61a956019c06cc333
syed-zunair-gillani <dev.zunaair@gmail.com>
2024-10-13 22:47:40 +0500

    weback

--- next.config.mjs
+++ next.config.mjs
@@ -1,34 +1,38 @@
 /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//     images: {
-//         remotePatterns: [
-//           {
-//             protocol: 'https',
-//             hostname: 'backend.vermap.com',
-//           },
-//         ],
-//     },
-// };
-
-// export default nextConfig;
-
-
-
-/** @type {import('next').NextConfig} */
-import withOptimizedImages from 'next-optimized-images';
-
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'backend.vermap.com',
-      },
-    ],
-  },
-  // Additional settings for next-optimized-images
-  handleImages: ['svg'],
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'backend.vermap.com',
+          },
+        ],
+    },
+    webpack(config) {
+      // Grab the existing rule that handles SVG imports
+      const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.(".svg"));
+  
+      config.module.rules.push(
+        // Reapply the existing rule, but only for svg imports ending in ?url
+        {
+          ...fileLoaderRule,
+          test: /\.svg$/i,
+          resourceQuery: /url/, // *.svg?url
+        },
+        // Convert all other *.svg imports to React components
+        {
+          test: /\.svg$/i,
+          issuer: fileLoaderRule.issuer,
+          resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
+          use: ["@svgr/webpack"],
+        },
+      );
+  
+      // Modify the file loader rule to ignore *.svg, since we have it handled now.
+      fileLoaderRule.exclude = /\.svg$/i;
+  
+      return config;
+    },
 };
 
-// Export the config wrapped with the plugin
-export default withOptimizedImages(nextConfig);
+export default nextConfig;
