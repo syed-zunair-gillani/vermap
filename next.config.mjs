8f94ba2eeb774b06f919b39b29b0914171ef41d8
syed-zunair-gillani <dev.zunaair@gmail.com>
2024-10-13 22:11:48 +0500

    svg

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,34 @@
 /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     images: {
+//         remotePatterns: [
+//           {
+//             protocol: 'https',
+//             hostname: 'backend.vermap.com',
+//           },
+//         ],
+//     },
+// };
+
+// export default nextConfig;
+
+
+
+/** @type {import('next').NextConfig} */
+import withOptimizedImages from 'next-optimized-images';
+
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'backend.vermap.com',
-          },
-        ],
-    },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'backend.vermap.com',
+      },
+    ],
+  },
+  // Additional settings for next-optimized-images
+  handleImages: ['svg'],
 };
 
-export default nextConfig;
+// Export the config wrapped with the plugin
+export default withOptimizedImages(nextConfig);
