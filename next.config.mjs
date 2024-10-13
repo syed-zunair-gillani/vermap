b75aa28e11d5e1e3d965840492f85664ad0d1f24
syed-zunair-gillani <dev.zunaair@gmail.com>
2024-10-13 22:53:12 +0500

    update

--- next.config.mjs
+++ next.config.mjs
@@ -8,25 +8,6 @@ const nextConfig = {
           },
         ],
     },
-    webpack(config, options) {
-      const fileLoaderRule = config.module.rules.find(rule => rule.test && rule.test.test('.svg'))
-      fileLoaderRule.exclude = /\.svg$/
-  
-      config.module.rules.push({
-        loader: '@svgr/webpack',
-        options: {
-          prettier: false,
-          svgo: true,
-          svgoConfig: {
-            plugins: [{ removeViewBox: false }],
-          },
-          titleProp: true,
-        },
-        test: /\.svg$/,
-      });
-  
-      return config;
-  }
 };
 
 export default nextConfig;
