0ca9afb5424e495c54239333fcfeafde98de8fa3
syed-zunair-gillani <dev.zunaair@gmail.com>
2024-10-13 23:06:13 +0500

    image

--- src/components/templates/RoomSlug.tsx
+++ src/components/templates/RoomSlug.tsx
@@ -111,7 +111,7 @@ const RoomSlug = ({ data }: any) => {
               <ul className="mt-4 grid gap-4 md:grid-cols-2">
                 {data?.acf?.room_amenities?.map((item: any, id: number) => (
                   <li key={id} className="flex items-center gap-2">
-                    <img
+                    <Image
                       src={`${item.icon}`}
                       alt={item.feature}
                       width={30}
