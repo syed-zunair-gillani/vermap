1e4126683b97b183f0658c840713829a3723118a
syed-zunair-gillani <dev.zunaair@gmail.com>
2024-10-13 22:51:47 +0500

    up

--- next.config.mjs
+++ next.config.mjs
@@ -8,31 +8,25 @@ const nextConfig = {
           },
         ],
     },
-    webpack(config) {
-      // Grab the existing rule that handles SVG imports
-      const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.(".svg"));
+    webpack(config, options) {
+      const fileLoaderRule = config.module.rules.find(rule => rule.test && rule.test.test('.svg'))
+      fileLoaderRule.exclude = /\.svg$/
   
-      config.module.rules.push(
-        // Reapply the existing rule, but only for svg imports ending in ?url
-        {
-          ...fileLoaderRule,
-          test: /\.svg$/i,
-          resourceQuery: /url/, // *.svg?url
+      config.module.rules.push({
+        loader: '@svgr/webpack',
+        options: {
+          prettier: false,
+          svgo: true,
+          svgoConfig: {
+            plugins: [{ removeViewBox: false }],
+          },
+          titleProp: true,
         },
-        // Convert all other *.svg imports to React components
-        {
-          test: /\.svg$/i,
-          issuer: fileLoaderRule.issuer,
-          resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
-          use: ["@svgr/webpack"],
-        },
-      );
-  
-      // Modify the file loader rule to ignore *.svg, since we have it handled now.
-      fileLoaderRule.exclude = /\.svg$/i;
+        test: /\.svg$/,
+      });
   
       return config;
-    },
+  }
 };
 
 export default nextConfig;
