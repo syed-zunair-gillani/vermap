4a17a4bac89dbff85f9e6aeef8692b85c37e88ff
syed-zunair-gillani <dev.zunaair@gmail.com>
2024-11-28 00:38:08 +0500

    update

--- src/components/gallery/ModelBox.tsx
+++ src/components/gallery/ModelBox.tsx
@@ -1,8 +1,6 @@
 "use client";
 import * as React from "react";
 import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
 import { GlobalContext } from "@/context/global-context";
 import Image from "next/image";
